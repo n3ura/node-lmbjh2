dd522ef2781f8d7ac2ee1e1af716ede827d440cd
n3ura <31388966+n3ura@users.noreply.github.com>
2023-08-19 22:29:48 +0200

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const db = require('./querys');
 const bodyparser = require('body-parser');
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
